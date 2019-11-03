50597589e96b8b04bb374a205951eca2bddf4de7
tamilla <tbunyadova@gmail.com>
2019-11-02 20:38:16 -0400

    Add tab service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TabsService } from './core/tabs.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    TabsService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/core/tabs.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { filter } from 'minimatch';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TabsService {
+  private hideTabBarPages: string[] = [
+    'info',
+  ];
+
+  routeParamPages: string[] = [
+    'safety-plan',
+    'resources'
+  ];
+
+  constructor(private router: Router, private platform: Platform) { 
+    this.platform.ready().then(() => {
+      this.navEvents();
+    });
+  }
+
+  private navEvents() {
+    this.router.events.subscribe(e => {
+      if(e instanceof NavigationEnd){
+        console.log(e);
+        this.showHideTabs(e);
+      }
+    });
+  }
+
+  private showHideTabs(e: any) {
+    const urlArray = e.url.split('/');
+    const pageUrlParent = urlArray[urlArray.length - 2];
+    const pageUrl = urlArray[urlArray.length - 1];
+    const page = pageUrl.split('?')[0];
+    const hideParamPage = this.routeParamPages.indexOf(pageUrlParent) > -1 && !isNaN(Number(page));
+    const shouldHide = this.hideTabBarPages.indexOf(page) > -1 || hideParamPage;
+    try {
+      setTimeout(() => shouldHide ? this.hideTabs() : this.showTabs(), 300);
+    } catch (err) {
+    }
+   }
+
+   public hideTabs() {
+    const tabBar = document.getElementById('myTabBar');
+    if (tabBar !== null && tabBar.style.display !== 'none') tabBar.style.display = 'none';
+  }
+  
+  public showTabs() {
+    const tabBar = document.getElementById('myTabBar');
+    if (tabBar !== null && tabBar.style.display !== 'flex') tabBar.style.display = 'flex';
+  }
+}
